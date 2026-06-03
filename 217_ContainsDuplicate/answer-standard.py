7c23687fa92c05ab3657af23f309cb553b80cbf8
Lim <yiksenglim6@gmail.com>
2026-06-03 19:12:28 +0800

    LeetCode217: Contains Duplicate, using set to solve instead of dict as there is no need to use the index of the input list

---
+++ 217_ContainsDuplicate/answer-standard.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if not nums:
+            return False
+        # set class in python
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
