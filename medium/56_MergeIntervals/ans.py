0e9422a77b68f90a9b4b15fd3ccb6c79e42fcc96
Lim <yiksenglim6@gmail.com>
2026-07-21 00:03:37 +0800

    LeetCode 56: Time: O(nlogn) in sorting + O(n) in checking intervals, Space: O(n); sort first then solve

---
+++ medium/56_MergeIntervals/ans.py
@@ -0,0 +1,15 @@
+### LeetCode 56: Time: O(nlogn) in sorting + O(n) in checking intervals, Space: O(n); sort first then solve
+from typing import List
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort(key=lambda x: x[0])
+        ans = []
+        ans.append(intervals[0])
+        for interval in intervals:
+            if interval[0] > ans[-1][1]:
+                ans.append(interval)
+            else: # interval[0] <= ans[-1][1]:
+                ans[-1][1] = max(ans[-1][1], interval[1])
+
+        return ans
